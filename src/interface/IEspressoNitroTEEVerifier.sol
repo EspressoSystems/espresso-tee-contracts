8d8b24ab66fbe8d2e05f3f6838d327d48b8382b0
Luke Iannucci <luke.iannucci@outlook.com>
2025-03-31 16:33:16 -0400

    cleanup

--- src/interface/IEspressoNitroTEEVerifier.sol
+++ src/interface/IEspressoNitroTEEVerifier.sol
@@ -2,21 +2,8 @@
 pragma solidity ^0.8.0;
 
 interface IEspressoNitroTEEVerifier {
-    // We only support version 3 for now
-    error InvalidHeaderVersion();
-    // This error is thrown when the enclave report fails to parse
-    error FailedToParseEnclaveReport();
-    // This error is thrown when the mrEnclave don't match
+    // This error is thrown when the PCR0 values don't match
     error InvalidEnclaveHash();
-    // This error is thrown when the reportDataHash doesn't match the hash signed by the TEE
-    error InvalidReportDataHash();
-    // This error is thrown when the reportData is too short
-    error ReportDataTooShort();
-    // This error is thrown when the data length is invalid
-    error InvalidDataLength();
-    // This error is thrown when the signer address is invalid
-    error InvalidSignerAddress();
-    error AttestationTooOld();
 
     event EnclaveHashSet(bytes32 enclaveHash, bool valid);
     event SignerRegistered(address signer, bytes32 enclaveHash);
