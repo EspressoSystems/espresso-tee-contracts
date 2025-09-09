05fea7d90ab63da5d61efeacd8fde31894a03310
Zach Showalter <zacshowa@gmail.com>
2025-09-09 17:02:52 -0400

    Update inner TEEVerifier contracts to have the required interface for registering multiple services

--- src/interface/IEspressoNitroTEEVerifier.sol
+++ src/interface/IEspressoNitroTEEVerifier.sol
@@ -1,23 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+import {Service} from "./IEspressoTEEVerifier.sol"
 interface IEspressoNitroTEEVerifier {
     // This error is thrown when the PCR0 values don't match
     error InvalidAWSEnclaveHash();
 
-    event AWSEnclaveHashSet(bytes32 enclaveHash, bool valid);
-    event AWSSignerRegistered(address signer, bytes32 enclaveHash);
-    event DeletedAWSRegisteredSigner(address signer);
+    event AWSEnclaveHashSet(bytes32 enclaveHash, bool valid, Service service);
+    event AWSServiceRegistered(address signer, bytes32 enclaveHash, Service service);
+    event DeletedAWSRegisteredService(address signer, Service service);
 
-    function registeredSigners(address signer) external view returns (bool);
+    //This will serve to check if a Caff node is regsitered in the NitroTEEVerifier contract.
+    function registeredCaffNode(address signer) external view returns (bool);
+
+    function registeredBatchPoster(address signer) external view returns (bool);
     function registeredEnclaveHash(bytes32 enclaveHash) external view returns (bool);
 
-    function registerSigner(bytes calldata attestation, bytes calldata data) external;
+    /*
+    * @notice This function is for registering AWS Nitro TEE Caff Nodes and is a helper function for the EspressoTEEVerifier
+    */
+    function registerCaffNode(bytes calldata attestation, bytes calldata data) external;
+
+    /*
+    * @notice This function is for registering AWS Nitro Batch Posters and is a helper function for the EspressoTEEVerifier
+    */
+    function registerBatchPoster(bytes calldata verificationData, bytes calldata data) external;
 
     function verifyCACert(bytes calldata certificate, bytes32 parentCertHash) external;
     function verifyClientCert(bytes calldata certificate, bytes32 parentCertHash) external;
     function certVerified(bytes32 certHash) external view returns (bool);
 
-    function setEnclaveHash(bytes32 enclaveHash, bool valid) external;
-    function deleteRegisteredSigners(address[] memory signers) external;
+    function setEnclaveHash(bytes32 enclaveHash, bool valid, ) external;
+    /*
+    * @notice This function is responsible for removing registered addresses from the list of valid Caff Nodes
+    */
+    function deleteRegisteredCaffNodes(address[] memory signers) external;
+    /*
+    * @notice This function is responsible for removing registered addresses from the list of valid Batch Posters
+    */
+    function deleteRegisteredBatchPosters(address[] memory signers) external;
 }
--- src/interface/IEspressoSGXTEEVerifier.sol
+++ src/interface/IEspressoSGXTEEVerifier.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {Header} from "@automata-network/dcap-attestation/contracts/types/CommonStruct.sol";
 import {EnclaveReport} from "@automata-network/dcap-attestation/contracts/types/V3Structs.sol";
+import {Service} from "./IEspressoTEEVerifier.sol";
 
 interface IEspressoSGXTEEVerifier {
     // We only support version 3 for now
@@ -12,7 +13,7 @@ interface IEspressoSGXTEEVerifier {
     // This error is thrown when the enclave report fails to parse
     error FailedToParseEnclaveReport();
     // This error is thrown when the mrEnclave don't match
-    error InvalidEnclaveHash();
+    error InvalidEnclaveHash(Service);
     // This error is thrown when the reportDataHash doesn't match the hash signed by the TEE
     error InvalidReportDataHash();
     // This error is thrown when the reportData is too short
@@ -24,14 +25,30 @@ interface IEspressoSGXTEEVerifier {
     // This error is thrown when the quote verifier address is invalid
     error InvalidQuoteVerifierAddress();
 
-    event EnclaveHashSet(bytes32 enclaveHash, bool valid);
-    event SignerRegistered(address signer, bytes32 enclaveHash);
-    event DeletedRegisteredSigner(address signer);
+    event EnclaveHashSet(bytes32 enclaveHash, bool valid, Service service);
+    event ServiceRegistered(address signer, bytes32 enclaveHash, Service service);
+    event DeletedRegisteredService(address signer, Service service);
 
-    function registeredSigners(address signer) external view returns (bool);
+    /*
+    * @notice This function is for checking the registration status of  Intel SGX TEE BatchPosters and is a helper function for the EspressoTEEVerifier
+    */
+    function registeredBatchPoster(address signer) external view returns (bool);
+
+    /*
+    * @notice This function is for checking the registration status of Intel SGX TEE Caff Nodes and is a helper function for the EspressoTEEVerifier
+    */
+    function registeredCaffNode(address signer) external view returns (bool);
+
     function registeredEnclaveHash(bytes32 enclaveHash) external view returns (bool);
 
-    function registerSigner(bytes calldata attestation, bytes calldata data) external;
+    /*
+    * @notice This function is for registering Intel SGX TEE Batch Posters and is a helper function for the EspressoTEEVerifier
+    */
+    function registerBatchPoster(bytes calldata attestation, bytes calldata data) external;
+    /*
+    * @notice This function is for checking the registration status of Intel SGX TEE BatchPosters and is a helper function for the EspressoTEEVerifier
+    */
+    function registerCaffNode(bytes calldata attestation, bytes calldata data) external;
 
     function verify(bytes calldata rawQuote, bytes32 reportDataHash)
         external
@@ -47,7 +64,16 @@ interface IEspressoSGXTEEVerifier {
         external
         pure
         returns (bool success, EnclaveReport memory enclaveReport);
-
-    function setEnclaveHash(bytes32 enclaveHash, bool valid) external;
-    function deleteRegisteredSigners(address[] memory signers) external;
+    /*
+    * @notice: This function is responsible for setting the validity of enclave hashes in this inner TEEVerifier contract, It will be 
+    */
+    function setEnclaveHash(bytes32 enclaveHash, bool valid, Service serviceType) external;
+    /*
+    * @notice This function is responsible for removing registered addresses from the list of valid Caff Nodes
+    */
+    function deleteRegisteredCaffNodes(address[] memory signers) external;
+    /*
+    * @notice This function is responsible for removing registered addresses from the list of valid Batch Posters
+    */
+    function deleteRegisteredBatchPosters(address[] memory signers) external;
 }
