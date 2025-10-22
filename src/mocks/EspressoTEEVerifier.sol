c778e647c17fe206d49907fbc97d23efb424395b
Zach Showalter <zacshowa@gmail.com>
2025-10-22 15:18:39 -0500

    Update mock EspressoTEEVerifier contract (#24)
    * Update mock contract to reflect interface changes
    
    Update the mock EspressoTEEVerifier contract to reflect updated
    interfaces. This will allow us to properly test the updates in the
    integration code base.
    
    * run forgt-fmt

--- src/mocks/EspressoTEEVerifier.sol
+++ src/mocks/EspressoTEEVerifier.sol
@@ -7,35 +7,49 @@ pragma solidity ^0.8.0;
  * @notice Contains the logic to verify a quote from the TEE and attest on-chain. It uses the V3QuoteVerifier contract
  *         to verify the quote. Along with some additional verification logic.
  */
+
 contract EspressoTEEVerifierMock {
     enum TeeType {
         SGX,
         NITRO
     }
 
-    mapping(address => bool) public registeredSigner;
+    enum ServiceType {
+        BatchPoster,
+        CaffNode
+    }
+
+    mapping(address => bool) public registeredServicesMap;
 
     constructor() {}
 
-    function verify(bytes calldata signature, bytes32 userDataHash, TeeType teeType)
-        external
-        view
-        returns (bool)
-    {
+    function verify(
+        bytes calldata signature,
+        bytes32 userDataHash,
+        TeeType teeType,
+        ServiceType service
+    ) external view returns (bool) {
         return true;
     }
 
-    function registerSigner(bytes calldata attestation, bytes calldata data, TeeType teeType)
-        external
-    {
+    function registerService(
+        bytes calldata attestation,
+        bytes calldata data,
+        TeeType teeType,
+        ServiceType service
+    ) external {
         // data length should be 20 bytes
         require(data.length == 20, "Invalid data length");
 
         address signer = address(uint160(bytes20(data[:20])));
-        registeredSigner[signer] = true;
+        registeredServicesMap[signer] = true;
     }
 
-    function registeredSigners(address signer, TeeType teeType) external view returns (bool) {
-        return registeredSigner[signer];
+    function registeredServices(address signer, TeeType teeType, ServiceType service)
+        external
+        view
+        returns (bool)
+    {
+        return registeredServicesMap[signer];
     }
 }
