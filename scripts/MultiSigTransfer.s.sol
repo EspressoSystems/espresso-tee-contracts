a0ee259d18fc5588c65c5a4d9eedc662f7c6fa5e
Zach Showalter <zacshowa@gmail.com>
2025-12-10 13:11:55 -0500

    Update MultiSigTransfer script to support proposing transactions
    Currently, this only supports ETH, ETH Sepolia, and ARB One. We are
    hoping to support Arb Sepolia in the future via extending safe-utils to
    utilize OnChainDen

--- scripts/MultiSigTransfer.s.sol
+++ scripts/MultiSigTransfer.s.sol
@@ -7,6 +7,7 @@ import {console2} from "forge-std/console2.sol";
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Safe} from "safe-utils/Safe.sol";
+import {Enum} from "safe-smart-account/common/Enum.sol";
 
 import {IEspressoTEEVerifier} from "../src/interface/IEspressoTEEVerifier.sol";
 import {IEspressoNitroTEEVerifier} from "../src/interface/IEspressoNitroTEEVerifier.sol";
@@ -23,6 +24,7 @@ contract MultiSigTransfer is Script {
     address newOwner;
     address teeVerifierAddress;
     address proposerAddress;
+    uint256 originalOwnerPrivateKey;
     string derivationPath;
 
     IEspressoTEEVerifier teeVerifier;
@@ -40,10 +42,17 @@ contract MultiSigTransfer is Script {
     address originalSGXTeeVerifierOwner;
     Ownable2Step ownable2StepSGXTeeVerifier;
 
+    // Event signaling that the script has successfully initiated ownership transfers for the given set of TEEVerifier contracts.
+    event AllOwnershipTransfersStarted(
+        address teeVerifier, address nitroVerifier, address sgxVerifier
+    );
+    
+    // This function is executed by the forge VM whenever the script is executed using forge script.
     function setUp() public {
         //Get transfer initiation env vars
         newOwner = vm.envAddress("MULTISIG_CONTRACT_ADDRESS");
         teeVerifierAddress = vm.envAddress("TEE_VERIFIER_ADDRESS");
+        originalOwnerPrivateKey = vm.envUint("PRIVATE_KEY");
         //Get transfer acceptance env vars
         derivationPath = vm.envString("LEDGER_DERIVATION_PATH");
         proposerAddress = vm.envAddress("PROPOSER_ADDRESS");
@@ -53,6 +62,7 @@ contract MultiSigTransfer is Script {
         setUpContractVars();
 
     }
+    
     // testSetUp is a helper function to handle the parts of the setUp() function related to the unit tests.
     // setUp() won't be called for this contract when run in a unit test context, so it's useful to have this to enable testing.
     function testSetUp() internal{
@@ -62,6 +72,7 @@ contract MultiSigTransfer is Script {
 
         setUpContractVars();
     }
+    
     // setUpContractVars() populates contract variables at the contract state level in order to compartmentalize this logic for unit tests and normal execution.
     function setUpContractVars() internal{
         teeVerifier = IEspressoTEEVerifier(teeVerifierAddress);
@@ -79,11 +90,6 @@ contract MultiSigTransfer is Script {
         originalSGXTeeVerifierOwner = ownableTeeVerifier.owner();
         ownable2StepSGXTeeVerifier = Ownable2Step(address(sgxVerifier));
     }
-    
-    // Event signaling that the script has successfully initiated ownership transfers for the given set of TEEVerifier contracts.
-    event AllOwnershipTransfersStarted(
-        address teeVerifier, address nitroVerifier, address sgxVerifier
-    );
 
     // initiateTransfer() is a helper function for this script that allows the
     // original owner of the TEEVerifier contracts to initiate ownership transfer to the multi-sig wallet.
@@ -91,8 +97,9 @@ contract MultiSigTransfer is Script {
     //        - If there is not an appropriate OwnershipTransferStarted event emitted by any of the TEEVerifier contracts
     function initiateTransfer() internal {
             
-        //After each step, we assert that the owner of the contract is still the original owner, but that the pending owner has updated.
+        // After each step, we assert that the owner of the contract is still the original owner, but that the pending owner has updated.
         // If this doesn't hold, the transaction reverts.
+        
         ownable2StepSGXTeeVerifier.transferOwnership(newOwner);
         assertTransferInitiated(address(sgxVerifier), originalSGXTeeVerifierOwner, newOwner);
 
@@ -106,48 +113,34 @@ contract MultiSigTransfer is Script {
             address(teeVerifier), address(nitroVerifier), address(sgxVerifier)
         );
     }
-    // addToBatch is a helper function for appending a transaction to the current batch being built for this ownership transfer.
-    // Return values:
-    //              - txHash: a bytes32 value representing the transaction hash proposed to the multi-sig wallet.
-    function addToBatch(address target, bytes[] callData) internal returns (bytes32) {
-        batchTargets.push(target);
-        batchData.push(callData);
-    }
 
-    function proposeOwnershipAcceptanceTransaction() internal{
-        //Generate transaction target and data arrays for signing.
-        bytes memory transactionDataSGX = abi.encodeCall(ownable2StepSGXTeeVerifier.acceptOwnership);
-        bytes memory transactionDataNitro = abi.encodeCall(ownable2StepNitroTeeVerifier.acceptOwnership);
-        bytes memory transactionData = abi.encodeCall(ownable2StepTeeVerifier.acceptOwnership);
+    // proposeOwnershipAcceptaceTransaction is a function that utilizes the safe-utils library and Safe.Client to
+    // interact with the safe-transaction-service API to propose a batch transaction for accepting ownership of the TEEVerifier contracts.
+    // Return values:
+    //              - bytes32 txHash: The transaction hash of the multi-sig transaction that was proposed to the web UI.
+    function proposeOwnershipAcceptanceTransaction() internal returns (bytes32){
+        // Generate transaction target and data arrays for signing.
+        bytes memory transactionDataSGX = abi.encodeCall(ownable2StepSGXTeeVerifier.acceptOwnership, ());
+        bytes memory transactionDataNitro = abi.encodeCall(ownable2StepNitroTeeVerifier.acceptOwnership, ());
+        bytes memory transactionData = abi.encodeCall(ownable2StepTeeVerifier.acceptOwnership, ());
 
         addToBatch(address(sgxVerifier), transactionDataSGX);
         addToBatch(address(nitroVerifier), transactionDataNitro);
         addToBatch(address(teeVerifier), transactionData);
 
-        //Get composed transaction data
-        (address to, bytes memory data) = safe.getProposeTransactionsTargetAndData(batchTargets, batchData);
-
-        // Get signature over transaction so we can add it to a batch of transactions
-        // Note: This is currently necessary because of some current limitations with safe-util (more accurately, forge itself) with using a ledger to sign transactions.
-        // The maintainers implemented a workaround that allows you to bypass this by signing the transaction ahead of time, so we do that here.
-        // The fix to the underlying issue in forge is slated for the 1.6.0 release currently, although it was already delayed from 1.5.0.
-        // Forge issue: https://github.com/foundry-rs/foundry/issues/10986
-        // Original safe-utils issue: https://github.com/Recon-Fuzz/safe-utils/issues/3 
-        // Original safe-utils issue with link to PR implementing workaround: https://github.com/Recon-Fuzz/safe-utils/issues/15
-        bytes memory signature = safe.sign(to, data, Enum.Operation.Call, proposerAddress, derivationPath);
-
-        //Use signature to propose transaction to the safe wallet
-        txHash = safe.proposeTransactionsWithSignature(
-            batchTargets,
-            batchData,
-            proposerAddress,
-            signature
-        );
-        return txHash;
+        return safe.proposeTransactions(batchTargets, batchData, proposerAddress, derivationPath);
         
     }
 
-    //
+    
+    // addToBatch is a helper function for appending a transaction to the current batch being built for this ownership transfer.
+    // Return values:
+    //              - txHash: a bytes32 value representing the transaction hash proposed to the multi-sig wallet.
+    function addToBatch(address target, bytes memory callData) internal {
+        batchTargets.push(target);
+        batchData.push(callData);
+    }
+
     // assertTransferInitiated is a helper function to handle asserting when an ownership transfer has been initiated on an Ownable2Step implementing contract
     // Params:
     //       - contractAddress: The address to a smart contract that implements Ownable, and Ownable2Step.
@@ -170,8 +163,8 @@ contract MultiSigTransfer is Script {
         );
     }
 
-    // testTransferEntrypoint() is a helper function that serves as a testing entry point for this script
-    // its purpose is to trick the vm into thinking that the test is not making cross-contract calls and overwriting msg.sender.
+    // testTransferEntrypoint() is a helper function that serves as a testing entry point for this script.
+    // It's purpose is to trick the vm into thinking that the test is not making cross-contract calls and overwriting msg.sender.
     // This allows us to spoof more accurate testing conditions, as this script won't ever be deployed/called on chain.
     function transferTestEntrypoint() public {
 
@@ -187,12 +180,18 @@ contract MultiSigTransfer is Script {
     // The main entrypoint for the script when run using `forge script`
     // This entrypoint will broadcast the transaction on chain.
     function run() external {
-        vm.startBroadcast();
+        // Start broadcast with original signers key
+        vm.startBroadcast(originalOwnerPrivateKey);
 
         initiateTransfer();
 
-        txHash = proposeOwnershipAcceptanceTransaction();
-        console2.log("multi-sig transaction hash:", txHash);
+        vm.stopBroadcast();
+
+        // Start broadcast with proposers address
+        vm.startBroadcast(proposerAddress);
+        bytes32 txHash = proposeOwnershipAcceptanceTransaction();
+        console2.log("multi-sig transaction hash:");
+        console2.logBytes32(txHash);
         
         vm.stopBroadcast();
     }
