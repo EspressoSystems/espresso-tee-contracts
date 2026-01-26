943e53bf9e18d96f4a7fba7e6612008bef3c77be
Artemii Gerasimovich <artemii@espressosys.com>
2026-01-26 17:32:20 +0100

    Formatting

--- src/OwnableWithGuardiansUpgradeable.sol
+++ src/OwnableWithGuardiansUpgradeable.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Ownable2StepUpgradeable} from
-    "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
-import {AccessControlEnumerableUpgradeable} from
-    "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
+import {
+    Ownable2StepUpgradeable
+} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import {
+    AccessControlEnumerableUpgradeable
+} from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
@@ -59,10 +61,7 @@ abstract contract OwnableWithGuardiansUpgradeable is
         __OwnableWithGuardians_init_unchained(initialOwner);
     }
 
-    function __OwnableWithGuardians_init_unchained(address initialOwner)
-        internal
-        onlyInitializing
-    {
+    function __OwnableWithGuardians_init_unchained(address initialOwner) internal onlyInitializing {
         // Grant the initial owner the default admin role for managing guardians
         _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
 
