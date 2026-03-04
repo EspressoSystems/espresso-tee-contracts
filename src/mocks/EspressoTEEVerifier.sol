c6cb42c73acd822de237d1dae5641b8d2548c602
Sneh Koul <snehkoul1999@gmail.com>
2026-03-04 12:29:02 +0530

    fix: update mock to use eip712

--- src/mocks/EspressoTEEVerifier.sol
+++ src/mocks/EspressoTEEVerifier.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {IEspressoTEEVerifier} from "../interface/IEspressoTEEVerifier.sol";
 import {IEspressoSGXTEEVerifier} from "../interface/IEspressoSGXTEEVerifier.sol";
 import {IEspressoNitroTEEVerifier} from "../interface/IEspressoNitroTEEVerifier.sol";
@@ -11,15 +12,20 @@ import {ServiceType} from "../types/Types.sol";
  * @title EspressoTEEVerifierMock
  * @notice Mock contract for TEE verification. Skips all attestation verification
  *         but still requires signers to be registered before they can be used.
+ *         Uses EIP-712 typed data signing to match the real EspressoTEEVerifier.
  */
-contract EspressoTEEVerifierMock {
+contract EspressoTEEVerifierMock is EIP712 {
     IEspressoSGXTEEVerifier public espressoSGXTEEVerifier;
     IEspressoNitroTEEVerifier public espressoNitroTEEVerifier;
+    mapping(address => uint256) public signerNonces;
+
+    bytes32 private constant ESPRESSO_TEE_VERIFIER_TYPE_HASH =
+        keccak256("EspressoTEEVerifier(bytes32 commitment,uint256 nonce)");
 
     constructor(
         IEspressoSGXTEEVerifier _espressoSGXTEEVerifier,
         IEspressoNitroTEEVerifier _espressoNitroTEEVerifier
-    ) {
+    ) EIP712("EspressoTEEVerifier", "1") {
         espressoSGXTEEVerifier = _espressoSGXTEEVerifier;
         espressoNitroTEEVerifier = _espressoNitroTEEVerifier;
     }
@@ -36,21 +42,24 @@ contract EspressoTEEVerifierMock {
         IEspressoTEEVerifier.TeeType teeType,
         ServiceType service
     ) external returns (bool) {
-        address signer = ECDSA.recover(userDataHash, signature);
+        uint256 addressNonce = signerNonces[msg.sender];
+        bytes32 structHash =
+            keccak256(abi.encode(ESPRESSO_TEE_VERIFIER_TYPE_HASH, userDataHash, addressNonce));
+        bytes32 digest = _hashTypedDataV4(structHash);
+        address signer = ECDSA.recover(digest, signature);
 
         if (teeType == IEspressoTEEVerifier.TeeType.SGX) {
             if (!espressoSGXTEEVerifier.isSignerValid(signer, service)) {
                 revert IEspressoTEEVerifier.InvalidSignature();
             }
-            return true;
-        }
-
-        if (teeType == IEspressoTEEVerifier.TeeType.NITRO) {
+        } else if (teeType == IEspressoTEEVerifier.TeeType.NITRO) {
             if (!espressoNitroTEEVerifier.isSignerValid(signer, service)) {
                 revert IEspressoTEEVerifier.InvalidSignature();
             }
-            return true;
         }
+
+        signerNonces[msg.sender] += 1;
+        return true;
     }
 
     /**
